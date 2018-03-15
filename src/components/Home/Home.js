849172010b192c431370df5f40495352f998fce9
Mike McCready <michaeljmccready@gmail.com>
2018-03-14 23:47:45 -0700

    refactor to stateful component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { AES, enc } from 'crypto-js'
+import { hashSecret as secret } from '../../../config.js'
 
-const Home = () => (
-  <div className="container">
-    <h1>Trainer</h1>
-    <Link to="/new-session">
-      <button onClick={() => this.setState({ newSession: true })}>
-        Record a Session
-      </button>
-    </Link>
-  </div>
-)
+export default class Home extends Component {
+  state = {
+    sessions: null
+  }
 
-export default Home
+  componentDidMount() {
+    this.retrieveSessions()
+  }
+
+  retrieveSessions() {
+    const storedHashed = localStorage.getItem('sessions')
+    if (storedHashed) {
+      const bytes = AES.decrypt(storedHashed.toString(), secret)
+      this.setState({ sessions: JSON.parse(bytes.toString(enc.Utf8)) })
+    }
+  }
+
+  render() {
+    const { sessions } = this.state
+
+    return (
+      <div className="container">
+        <h1>Trainer</h1>
+
+        {
+          sessions &&
+          <h3>{ sessions.length } Sessions</h3>
+        }
+
+        <Link to="/new-session">
+          <button onClick={() => this.setState({ newSession: true })}>
+            Record a Session
+          </button>
+        </Link>
+      </div>
+    )
+  }
+}
